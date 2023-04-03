dc004d8edfafab70bb78df201099336ca7ecfb9f
Abdul Rafeh <abdulrafeh857@gmail.com>
2023-04-04 03:30:51 +0500

    Update curvedView.tsx

--- src/CurvedBottomBar/components/CurvedView/curvedView.tsx
+++ src/CurvedBottomBar/components/CurvedView/curvedView.tsx
@@ -1,16 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { Path, Svg } from 'react-native-svg';
 import { CurvedBottomBarView } from '../ShadowView';
 import { CurvedView } from './model';
+import { View } from 'react-native';
 
 export const CurvedViewComponent: CurvedView = (props) => {
-  const { style, width, height, bgColor, path } = props;
+  const { style, width, height, bgColor, path, stroke, strokeWidth } = props;
 
   return (
-    <CurvedBottomBarView style={style}>
-      <Svg width={width} height={height}>
-        <Path fill={bgColor} {...{ d: path }} />
-      </Svg>
-    </CurvedBottomBarView>
+    <View
+      style={{
+        backgroundColor: 'transparent',
+      }}
+    >
+      <CurvedBottomBarView style={style}>
+        <Svg width={width} height={height}>
+          <Path
+            fill={bgColor}
+            stroke={stroke}
+            strokeWidth={strokeWidth}
+            {...{ d: path }}
+          />
+        </Svg>
+      </CurvedBottomBarView>
+    </View>
   );
 };
